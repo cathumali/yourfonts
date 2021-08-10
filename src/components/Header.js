baa6003cb75e25ac2676e1fec9b3413e98eeedc9
rinagreen <cathrinaumali@gmail.com>
2021-08-11 07:13:59 +0800

    added id on the data

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ const Header = (props) => {
     return (<>
         <div className="d-flex align-items-end flex-column">   
             <div className="svg-cart">
-            <svg className="w-6 h-6 " fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"></path></svg>
+            <svg className="w-6 h-6 " fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"></path></svg>
             </div>
         </div>
         <header className="d-flex align-items-center pb-3 mb-5 border-bottom">
